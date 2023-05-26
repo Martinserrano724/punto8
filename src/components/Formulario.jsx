9de1b888b8ac1bc41f71d6507d4c74ee50f51b91
Martinserrano724 <martinserrano724@gmail.com>
2023-05-26 00:25:38 -0300

    Desarrollo de la logica de de validaciones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,11 +3,13 @@ import { Form, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import Swal from 'sweetalert2'
 
+
 const Formulario = () => {
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
   const [dni, setDni] = useState("");
   const [mail, setMail] = useState("");
+ let valicacionForm='d-none bg-primary'
 
   const valicacionNombreApellido = (texto) => {
     if (texto != "" && texto.length < 100) {
@@ -52,53 +54,81 @@ const Formulario = () => {
             icon: 'success',
           })
     } else {
-      alert(validaciones());
+      Swal.fire({
+        title: 'ERROR',
+        text: `${validaciones()}`,
+        icon: 'error',
+      })
     }
+
+    
   };
   return (
-    <div>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
+    <div className="d-flex justify-content-center d-inline">
+    
+      <div className=" contenedorFormulario">
+        <div>
+        <h3 className="text-center tituloFormulario">Formulario</h3>
+        </div>
+     
+      <Form onSubmit={handleSubmit} className="">
+        <Form.Group className="mb-2 p-1">
           <Form.Label>Nombre </Form.Label>
           <Form.Control
             type="text"
             placeholder="ingrese un Nombre"
             onChange={(e) => setNombre(e.target.value)}
             value={nombre}
+            required
+            minLength={1}
+            maxLength={100}
+            
+          
           />
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-2 ps-1 pe-1">
           <Form.Label>Apellido</Form.Label>
           <Form.Control
             type="text"
             placeholder="ingrese un Apellido"
             onChange={(e) => setApellido(e.target.value)}
             value={apellido}
+            required
+            minLength={1}
+            maxLength={100}
+            
           />
+          
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-2 ps-1 pe-1">
           <Form.Label>Dni</Form.Label>
           <Form.Control
             type="number"
             placeholder="ingrese un Dni"
             onChange={(e) => setDni(e.target.value)}
             value={dni}
+            required
+            min={1000000}
+            max={99999999}
           />
         </Form.Group>
-        <Form.Group className="mb-3">
+        <Form.Group className="mb-2 ps-1 pe-1">
           <Form.Label>Mail</Form.Label>
           <Form.Control
             type="email"
             placeholder="ingrese un Mail"
             onChange={(e) => setMail(e.target.value)}
             value={mail}
+            required
           />
         </Form.Group>
-
-        <Button variant="primary" type="submit">
+<div className="d-flex justify-content-center">
+        <Button className="" type="submit">
           Submit
-        </Button>
+        </Button></div>
       </Form>
+ 
+      </div>
     </div>
   );
 };
